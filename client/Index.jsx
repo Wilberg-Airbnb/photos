fabb48d058a5423eb32cec8190eac98b263c8960
Eric Zepeda <zepedaeric@gmail.com>
2020-07-09 15:41:14 -0700

    Work on conditionally rendering setting default to cute puppers

--- client/Index.jsx
+++ client/Index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import MainPhotoCard from './components/PhotoCards/MainPhotoCard.jsx';
 import Photogrid from './components/PhotogridComponents/Photogrid.jsx';
 class Index extends React.Component {
   constructor(props) {
@@ -19,7 +18,6 @@ class Index extends React.Component {
   render() {
     return (
       <div>
-        {/* <MainPhotoCard></MainPhotoCard> */}
         <Photogrid photos={this.state.photos}></Photogrid>
       </div>
     );
--- client/components/PhotogridComponents/Photogrid.jsx
+++ client/components/PhotogridComponents/Photogrid.jsx
@@ -13,6 +13,9 @@ const GridContainer = styled.div`
 
 const Big = styled.div`
   background-color: pink;
+  background-image: url(${(props) =>
+    props.photo ||
+    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
   border: 1px solid rgba(0, 0, 0, 0.8);
   grid-area: 1 / 1 / 3 / 3;
   border-top-left-radius: 15px;
@@ -22,18 +25,27 @@ const Big = styled.div`
 const Photo1 = styled.div`
   grid-area: 1 / 3 / 2 / 4;
   background-color: pink;
+  background-image: url(${(props) =>
+    props.photo ||
+    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
   border: 1px solid rgba(0, 0, 0, 0.8);
 `;
 
 const Photo2 = styled.div`
   grid-area: 2 / 3 / 3 / 4;
   background-color: pink;
+  background-image: url(${(props) =>
+    props.photo ||
+    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
   border: 1px solid rgba(0, 0, 0, 0.8);
 `;
 
 const Photo3 = styled.div`
   grid-area: 1 / 4 / 2 / 5;
   background-color: pink;
+  background-image: url(${(props) =>
+    props.photo ||
+    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
   border: 1px solid rgba(0, 0, 0, 0.8);
   border-top-right-radius: 15px;
 `;
@@ -41,6 +53,9 @@ const Photo3 = styled.div`
 const Photo4 = styled.div`
   grid-area: 2 / 4 / 3 / 5;
   background-color: pink;
+  background-image: url(${(props) =>
+    props.photo ||
+    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
   border: 1px solid rgba(0, 0, 0, 0.8);
   border-bottom-right-radius: 15px;
 `;
@@ -52,6 +67,14 @@ grid-row-end: 3;
 `;
 
 const Photogrid = ({ photos }) => {
+  let collectionOfPhotos;
+
+  // Wait for the aysnc componentDidMount (for photos prop not to be null)
+  // and set the photos property
+  if (photos !== null) {
+    collectionOfPhotos = photos.photos;
+    console.log('Here are the photos', collectionOfPhotos);
+  }
   return (
     <GridContainer>
       <Big>1</Big>
