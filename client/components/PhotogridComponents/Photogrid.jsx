0559f49cb9e40b1b687a8d18d50704809a68c46a
Eric Zepeda <zepedaeric@gmail.com>
2020-07-11 12:20:32 -0700

    Change width of container

--- client/components/PhotogridComponents/Photogrid.jsx
+++ client/components/PhotogridComponents/Photogrid.jsx
@@ -7,7 +7,7 @@ const GridContainer = styled.div`
   grid-template-rows: repeat(5, 1fr);
   grid-column-gap: 8px;
   grid-row-gap: 8px;
-  max-width: 900px;
+  max-width: 950px;
   height: 1000px;
 `;
 
