8bf1496212adff2f6dafe902a59230d33a0d4b9e
Eric Zepeda <zepedaeric@gmail.com>
2020-07-15 23:38:52 -0700

    Work on displaying modal when button is clicked

---
+++ client/components/Modals/PhotoModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+let WholePage = styled.div`
+  height: 100vh;
+  background-color: green;
+`;
+
+const PhotoModal = () => {
+  return <WholePage></WholePage>;
+};
+
+export default PhotoModal;
