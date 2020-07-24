180e37d4229fc77d8aafc55f46c3b5f569a4c6ff
Eric Zepeda <zepedaeric@gmail.com>
2020-07-24 15:10:00 -0700

    Edit yml to change mongo name

--- database/connection.js
+++ database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://mongo:27017/fec-photos';
+const mongoUri = 'mongodb://mongo:37017/fec-photos';
 const db = mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
