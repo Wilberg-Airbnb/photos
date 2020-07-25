3dc53ae19163b7a7ba307e63f3b54601b9dbbfb1
Eric Zepeda <zepedaeric@gmail.com>
2020-07-24 20:35:02 -0700

    Remove photo

--- client/components/PhotogridComponents/Photogrid.jsx
+++ client/components/PhotogridComponents/Photogrid.jsx
@@ -232,7 +232,6 @@ class Photogrid extends React.Component {
             </Modal>
           ) : null}
         </GridContainer>
-        <img src="https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ"></img>
       </ContainerDiv>
     );
   }
