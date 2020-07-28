6be8c3d5af9d3da37e8387b1539ecdc46a4c5e8c
Eric Zepeda <zepedaeric@gmail.com>
2020-07-28 00:03:59 -0700

    Take care of picsum GET reqeusts on backend

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('../database/connection');
 const cors = require('cors');
 const Photos = require('../database/Photos');
+const fetch = require('node-fetch');
 
 const app = express();
 app.use(cors());
@@ -25,7 +26,28 @@ app.get('/api/photos/thumbnail/:listingId', (req, res) => {
 app.get('/api/photos/:listingId', (req, res) => {
   Photos.find({ listingId: req.params.listingId })
     .then((result) => {
-      res.json(result);
+      // Grab just the url;
+      return result[0].photos.map((photoObject) => photoObject.photoUrl);
+    })
+    .then((results) => {
+      // Promisify the array of lorem picsum urls to get to get the photo URL
+      // So that the front end isn't doing multiple GET requests to picusum and then
+      // the redirected source URL
+      return Promise.all(
+        results.map((request) => {
+          return fetch(request)
+            .then((response) => {
+              return response.url;
+            })
+            .then((urls) => {
+              return urls;
+            });
+        })
+      );
+    })
+    .then((urls) => {
+      console.log('Here are the urls', urls);
+      res.json(urls);
     })
     .catch((error) => {
       console.log('Error finding', error);
