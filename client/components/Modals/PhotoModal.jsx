3ebb815cb025aaaea91dd4497cdbb552438efaf4
Eric Zepeda <zepedaeric@gmail.com>
2020-07-29 20:22:59 -0700

    Specify CSS for photos service

--- client/components/Modals/PhotoModal.jsx
+++ client/components/Modals/PhotoModal.jsx
@@ -6,72 +6,88 @@ import styled from 'styled-components';
 const AWS_URL = `https://rpt21-airbrb-description.s3-us-west-1.amazonaws.com/`;
 
 const Icon = styled.img`
-  max-height: 12px;
+  #photo-modal & {
+    max-height: 12px;
+  }
 `;
 
 let WholePage = styled.div`
-  position: fixed;
-  padding: 0;
-  margin: 0;
-
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: 100%;
-  background: white;
+  #photo-modal & {
+    position: fixed;
+    padding: 0;
+    margin: 0;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: white;
+  }
 `;
 
 let Photo = styled.div`
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: 50%;
-  background-color: #fff;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  position: fixed;
-  margin: auto;
-  width: 75%;
-  min-width: 700px;
-  height: 75%;
-  background-image: url(${(props) =>
-    props.photo ||
-    'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
+  #photo-modal & {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50%;
+    background-color: #fff;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    position: fixed;
+    margin: auto;
+    width: 75%;
+    min-width: 700px;
+    height: 75%;
+    background-image: url(${(props) =>
+      props.photo ||
+      'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'});
+  }
 `;
-const Button = styled.button``;
+const Button = styled.button`
+#photomodal & [
+]`;
 
 const CloseButton = styled.button`
-  text-align: center;
-  border-radius: 5px;
-  padding: 6px 8px 6px 8px;
-  background-color: rgb(255, 255, 255);
-  font-size: 10px;
-  border-color: rgb(34, 34, 34);
-  margin-left: 10px;
+  #photo-modal & {
+    text-align: center;
+    border-radius: 5px;
+    padding: 6px 8px 6px 8px;
+    background-color: rgb(255, 255, 255);
+    font-size: 10px;
+    border-color: rgb(34, 34, 34);
+    margin-left: 10px;
+  }
 `;
 
 const Count = styled.span`
-  text-align: center;
+  #photo-modal & {
+    text-align: center;
+  }
 `;
 
-const PhotoRecord = styled.div``;
+const PhotoRecord = styled.div`
+#photomodal & [
+]`;
 
 const UpperContainer = styled.div`
-  display: inline-flex;
-  margin-top: 2.5%;
-  justify-content: space-between;
-  width: 96%;
+  #photo-modal & {
+    display: inline-flex;
+    margin-top: 2.5%;
+    justify-content: space-between;
+    width: 96%;
+  }
 `;
 
 const ButtonContainer = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  width: 95%;
-  height: 100%;
-  align-items: center;
+  #photo-modal & {
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    width: 95%;
+    height: 100%;
+    align-items: center;
+  }
 `;
 
 class PhotoModal extends React.Component {
